fb44f9f44e7133e6cb03af07853752f0d0d72f05
juan carlos <juan carlos@LAPTOP-O0H18VAV>
2024-02-12 07:48:28 -0600

    Numeros al cudrado

---
+++ src/cuadrados/Cuadrados.java
@@ -0,0 +1,23 @@
+
+package cuadrados;
+
+import javax.swing.JOptionPane;
+
+public class Cuadrados {
+
+   
+    public static void main(String[] args) {
+        
+     int numero,cuadrado;
+     numero=Integer.parseInt(JOptionPane.showInputDialog("Digita un numero"));
+     while(numero>=0)
+     {
+         cuadrado=(int)Math.pow( numero,2);
+         JOptionPane.showMessageDialog(null,"El cuadrodo del numero es: "+cuadrado);
+         numero=Integer.parseInt(JOptionPane.showInputDialog("Digita un numero"));
+     }
+        
+        
+    }
+    
+}
